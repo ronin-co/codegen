992327589f9c6dc8217bf31cbecfd131d2089089
Ben <4991309+NuroDev@users.noreply.github.com>
2025-05-16 17:55:52 +0100

    Minor type generator refactoring

--- src/generators/types.ts
+++ src/generators/types.ts
@@ -57,79 +57,6 @@ export const generateTypes = (
       (field) =>
         field.type === 'link' && models.some((model) => model.slug === field.target),
     );
-    const mappedModelFields = fields
-      .sort((a, b) => a.slug.localeCompare(b.slug))
-      .map((field) => {
-        const propertyUnionTypes = new Array<TypeNode>();
-
-        switch (field.type) {
-          case 'link': {
-            // Check to make sure the target model exists. If it doesn't we
-            // fall back to using `unknown` as the type.
-            const targetModel = models.find((model) => model.slug === field.target);
-            if (!targetModel) {
-              propertyUnionTypes.push(
-                factory.createKeywordTypeNode(SyntaxKind.UnknownKeyword),
-              );
-              break;
-            }
-
-            // If the field is marked as `many` then we need to wrap the
-            // type in an array.
-            const schemaTypeRef = factory.createTypeReferenceNode(
-              convertToPascalCase(targetModel.slug),
-            );
-            const resolvedLinkFieldNode = factory.createTypeReferenceNode(
-              identifiers.utils.resolveSchema,
-              [
-                field.kind === 'many'
-                  ? factory.createTypeReferenceNode(identifiers.primitive.array, [
-                      schemaTypeRef,
-                    ])
-                  : schemaTypeRef,
-
-                factory.createTypeReferenceNode(genericIdentifiers.using),
-
-                factory.createLiteralTypeNode(factory.createStringLiteral(field.slug)),
-              ],
-            );
-
-            propertyUnionTypes.push(resolvedLinkFieldNode);
-            break;
-          }
-          case 'blob':
-          case 'boolean':
-          case 'date':
-          case 'json':
-          case 'number':
-          case 'string': {
-            const primitive = MODEL_TYPE_TO_SYNTAX_KIND_KEYWORD[field.type];
-            propertyUnionTypes.push(primitive);
-            break;
-          }
-          default: {
-            propertyUnionTypes.push(
-              factory.createKeywordTypeNode(SyntaxKind.UnknownKeyword),
-            );
-            break;
-          }
-        }
-
-        // We need to mark fields as nullable if they are:
-        // - Not required
-        // - Not a link field
-        // - Not a many-to-many link field
-        if (field.required === false && field.type === 'link' && field.kind !== 'many')
-          propertyUnionTypes.push(factory.createLiteralTypeNode(factory.createNull()));
-
-        return factory.createPropertySignature(
-          undefined,
-          field.slug,
-          undefined,
-          factory.createUnionTypeNode(propertyUnionTypes),
-        );
-      })
-      .filter(Boolean) as Array<PropertySignature>;
 
     const modelInterfaceTypeParameters = new Array<TypeParameterDeclaration>();
     const linkFieldKeys = fields
@@ -184,7 +111,89 @@ export const generateTypes = (
           identifiers.syntax.resultRecord,
           undefined,
         ),
-        factory.createTypeLiteralNode(mappedModelFields),
+        factory.createTypeLiteralNode(
+          fields
+            .sort((a, b) => a.slug.localeCompare(b.slug))
+            .map((field) => {
+              const propertyUnionTypes = new Array<TypeNode>();
+
+              switch (field.type) {
+                case 'link': {
+                  // Check to make sure the target model exists. If it doesn't we
+                  // fall back to using `unknown` as the type.
+                  const targetModel = models.find((model) => model.slug === field.target);
+                  if (!targetModel) {
+                    propertyUnionTypes.push(
+                      factory.createKeywordTypeNode(SyntaxKind.UnknownKeyword),
+                    );
+                    break;
+                  }
+
+                  // If the field is marked as `many` then we need to wrap the
+                  // type in an array.
+                  const schemaTypeRef = factory.createTypeReferenceNode(
+                    convertToPascalCase(targetModel.slug),
+                  );
+                  const resolvedLinkFieldNode = factory.createTypeReferenceNode(
+                    identifiers.utils.resolveSchema,
+                    [
+                      field.kind === 'many'
+                        ? factory.createTypeReferenceNode(identifiers.primitive.array, [
+                            schemaTypeRef,
+                          ])
+                        : schemaTypeRef,
+
+                      factory.createTypeReferenceNode(genericIdentifiers.using),
+
+                      factory.createLiteralTypeNode(
+                        factory.createStringLiteral(field.slug),
+                      ),
+                    ],
+                  );
+
+                  propertyUnionTypes.push(resolvedLinkFieldNode);
+                  break;
+                }
+                case 'blob':
+                case 'boolean':
+                case 'date':
+                case 'json':
+                case 'number':
+                case 'string': {
+                  const primitive = MODEL_TYPE_TO_SYNTAX_KIND_KEYWORD[field.type];
+                  propertyUnionTypes.push(primitive);
+                  break;
+                }
+                default: {
+                  propertyUnionTypes.push(
+                    factory.createKeywordTypeNode(SyntaxKind.UnknownKeyword),
+                  );
+                  break;
+                }
+              }
+
+              // We need to mark fields as nullable if they are:
+              // - Not required
+              // - Not a link field
+              // - Not a many-to-many link field
+              if (
+                field.required === false &&
+                field.type === 'link' &&
+                field.kind !== 'many'
+              )
+                propertyUnionTypes.push(
+                  factory.createLiteralTypeNode(factory.createNull()),
+                );
+
+              return factory.createPropertySignature(
+                undefined,
+                field.slug,
+                undefined,
+                factory.createUnionTypeNode(propertyUnionTypes),
+              );
+            })
+            .filter(Boolean),
+        ),
       ]),
     );
 
